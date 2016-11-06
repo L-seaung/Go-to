395a8c26ea2c4a9ffff893ad73b6e70406d21f0c
L-seaung <2968055492@qq.com>
2016-11-07 02:18:04 +0800

    Go in superset and childset
    update interface

--- Interface.go
+++ Interface.go
@@ -14,9 +14,41 @@ func (s Student)SayHi(){
   io.Println("hi, my name is %s.", s.name)
 }
 
+type Tearch struct{
+  id uint32
+}
+
+type Manager struct{
+  Tearch
+  manager_name string
+}
+
+type TearchInfo interface{
+  slary()
+}
+
+type ManagerInfo interface{
+  slary()
+  managerInfo()
+}
+
+func (t Tearch)slary(){
+  io.Println(t)
+}
+
+func (m Manager) managerInfo(){
+  io.Println(m)
+}
+
 func main(){
   s := Student{"lee"}
   var speaker Speaker
   speaker = s
   speaker.SayHi()
+  var mn ManagerInfo = Manager{Tearch{123}, "seaung"}
+  mn.managerInfo()
+  mn.slary()
+  var te Tearch = mn
+  te.managerInfo()
+  te.slary()
 }
